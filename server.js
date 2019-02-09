5d05cc7cff93e2395ca7f68a6f842087b3928944
codeunifier <eva15023@byui.edu>
2019-02-09 09:20:36 -0700

    added static file for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const parser = require('body-parser');
+const path = require('path');
 const port = process.env.PORT || 8080;
 
 app.use(parser.json());
@@ -12,8 +13,8 @@ app.route("/api/test").get((req, res) => {
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/cryptoflow/index.html'));
 });
 
 app.get((req, res, next) => {
