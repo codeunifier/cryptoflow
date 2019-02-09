d55d2f2d99e7389cf23155d83db11e0c85265ad3
codeunifier <eva15023@byui.edu>
2019-02-09 09:00:32 -0700

    adding post-install build for production environment

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const app = express();
 const parser = require('body-parser');
-const PORT = 8080
+const port = process.env.PORT || 8080;
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
-
-app.listen(PORT, () => {
-    console.log("Server started at port " + PORT);
-});
 
 app.route("/api/test").get((req, res) => {
     res.status(200).send("Hello from server!");
 });
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
 app.get((req, res, next) => {
     next();
+});
+
+app.listen(port, () => {
+    console.log("Server started at port " + port);
 });
