09b391a100a133517906b2f2e6b15acfcdcae5bb
codeunifier <eva15023@byui.edu>
2019-10-19 09:07:02 -0700

    fixed issue with start/end dates in python script

--- server/routes/python.js
+++ server/routes/python.js
@@ -5,7 +5,7 @@ var request = require('request');
 const {spawn} = require("child_process");
 
 function formatDateString(date) {
-    let month = date.getMonth() < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
+    let month = date.getMonth() < 9 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
     let day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
 
     return date.getFullYear() + "-" + month + "-" + day;
